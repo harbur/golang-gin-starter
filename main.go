13a95b63a1b19327f0dfe4b4862eb37c6014254c
spiddy <dimitris.kapanidis@harbur.io>
2019-06-02 12:34:33 +0200

    feat: example of custom metrics

--- main.go
+++ main.go
@@ -7,7 +7,22 @@ import (
 	log "github.com/sirupsen/logrus"
 
 	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	version = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "version",
+		Help: "Version information about this binary",
+		ConstLabels: map[string]string{
+			"version": "v0.1.0",
+		},
+	})
+	httpRequestsTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "http_requests_total",
+		Help: "Count of all HTTP requests",
+	}, []string{"code", "method"})
 )
 
 func main() {
@@ -18,13 +33,16 @@ func main() {
 	log.SetLevel(log.DebugLevel)
 	log.SetOutput(os.Stdout)
 	log.Info("starting server")
+	r := prometheus.NewRegistry()
+	r.MustRegister(httpRequestsTotal)
+	r.MustRegister(version)
 
-	r := mux.NewRouter()
-	r.Handle("/metrics", promhttp.Handler())
-	r.HandleFunc("/{rest:.*}", handlerOK).Methods("GET")
+	m := mux.NewRouter()
+	m.Handle("/metrics", promhttp.HandlerFor(r, promhttp.HandlerOpts{}))
+	m.Handle("/{rest:.*}", promhttp.InstrumentHandlerCounter(httpRequestsTotal, http.HandlerFunc(handlerOK))).Methods("GET")
 
 	// Listen and Serve
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(":8080", m))
 }
 
 func handlerOK(w http.ResponseWriter, r *http.Request) {
